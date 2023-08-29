4c6df4c6c38e19f2471b7ad96fcdec85ed151669
oakekywe <oakekywe.zotefamily@gmail.com>
2023-08-29 11:09:32 +0630

    [fix] key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,7 +4,7 @@ import "firebase/compat/auth";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apikey,
+  apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
